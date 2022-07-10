79fd1a3ff6532a44e3ccf414802a71a2ed99f51f
neha <aneha0337@gmail.com>
2022-07-11 00:20:23 +0530

    Added seat registration form

--- myscript.js
+++ myscript.js
@@ -1,1 +1,7 @@
-
+function handleSubmit()
+{
+    alert(`Your Name: ${document.forms["registerForm"]["fname"].value}\n` + 
+    `No of people: ${document.forms["registerForm"]["people"].value}\n` + 
+    `Date: ${document.forms["registerForm"]["date"].value}\n` +
+    `Message: ${document.forms["registerForm"]["msg"].value}`);
+}
